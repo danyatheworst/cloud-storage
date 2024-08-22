e860b70bdc7801fea534ddd7e33458b40d5412cf
danyatheworst <danyatheworst@gmail.com>
2024-08-22 18:27:58 +0300

    fix(controller): sign up mapping get -> post

--- src/main/java/danyatheworst/auth/AuthController.java
+++ src/main/java/danyatheworst/auth/AuthController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final RegistrationService registrationService;
 
-    @GetMapping("/sign-up")
+    @PostMapping("/sign-up")
     public ResponseEntity<Void> signUp(@RequestBody @Valid RequestSignUpDto signUpDto) {
         this.registrationService.register(signUpDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
