8a0b48ea70e8ba39ffa2c42ede61374d25fcb2bd
danyatheworst <danyatheworst@gmail.com>
2024-09-09 09:34:46 +0300

    chore: unused import statements

--- src/test/java/danyatheworst/storage/FileStorageIntegrationTests.java
+++ src/test/java/danyatheworst/storage/FileStorageIntegrationTests.java
@@ -5,8 +5,6 @@ import danyatheworst.user.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
