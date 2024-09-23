4330fd4d13789b5b1b935cbc48b9a38f90186f5c
danyatheworst <danyatheworst@gmail.com>
2024-09-23 09:25:24 +0300

    chore: unused import statement

--- src/main/java/danyatheworst/config/SecurityConfiguration.java
+++ src/main/java/danyatheworst/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package danyatheworst.config;
 
 import danyatheworst.user.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
