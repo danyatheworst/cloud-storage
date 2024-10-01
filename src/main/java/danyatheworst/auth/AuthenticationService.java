908923afc76988e5959111fbf7c8f2e62eacf94b
danyatheworst <danyatheworst@gmail.com>
2024-10-01 11:41:44 +0300

    fix: bad credential message when password is wrong

--- src/main/java/danyatheworst/auth/AuthenticationService.java
+++ src/main/java/danyatheworst/auth/AuthenticationService.java
@@ -1,8 +1,11 @@
 package danyatheworst.auth;
 
 import danyatheworst.auth.dto.RequestSignInDto;
+import danyatheworst.exceptions.InvalidCredentialsException;
+import danyatheworst.exceptions.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,11 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public Authentication authenticate(RequestSignInDto signInDto) {
-        Authentication auth = new UsernamePasswordAuthenticationToken(signInDto.getUsername(), signInDto.getPassword());
-        return this.authenticationManager.authenticate(auth);
+        try {
+            Authentication auth = new UsernamePasswordAuthenticationToken(signInDto.getUsername(), signInDto.getPassword());
+            return this.authenticationManager.authenticate(auth);
+        } catch (BadCredentialsException e) {
+            throw new InvalidCredentialsException("Invalid username or password");
+        }
     }
 }
